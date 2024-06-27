7422533848c0fe53f2da61ce2e43655846d0d825
netplwiz <netplwiz@DESKTOP-TO3THAB>
2024-06-27 21:36:25 +0900

    06.27 21:35 productService

--- duckGduckG/src/main/java/com/duckG/product/service/productServiceImpl.java
+++ duckGduckG/src/main/java/com/duckG/product/service/productServiceImpl.java
@@ -1,5 +1,13 @@
 package com.duckG.product.service;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.duckG.product.mapper.productMapper;
+
+import co.yedam.common.DetaSource;
+
 public class productServiceImpl implements productService{
+	SqlSession sqlSession = DetaSource.getInstance().openSession(true);
+	productMapper mapper = sqlSession.getMapper(productMapper.class);
 
 }
