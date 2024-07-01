4f37ea56b0a9fac9b39fcc4a10e0811c9955a05d
admin <admin@YD01-11>
2024-07-01 17:43:04 +0900

    07.01 17:42 chc

--- duckGduckG/src/main/java/com/duckG/qna/controller/DeleteQnA.java
+++ duckGduckG/src/main/java/com/duckG/qna/controller/DeleteQnA.java
@@ -5,15 +5,30 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.duckG.Control;
+import com.duckG.product.service.ProductService;
+import com.duckG.product.service.ProductServiceImpl;
+import com.duckG.qna.mapper.QnAMapper;
+import com.duckG.qna.service.QnAService;
+import com.duckG.qna.service.QnAServiceImpl;
 
 public class DeleteQnA implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
+		QnAService svc = new QnAServiceImpl();
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/json;charset=UTF-8");
+		
+		int qnaNo = Integer.parseInt(req.getParameter("qnaNo"));
+		
+		HttpSession session = req.getSession();
+		String uid = (String)session.getAttribute("");
+		
+		resp.getWriter().print(svc.delQnA(qnaNo));
 	}
 
 }
--- duckGduckG/src/main/java/com/duckG/qna/service/QnAService.java
+++ duckGduckG/src/main/java/com/duckG/qna/service/QnAService.java
@@ -1,5 +1,17 @@
 package com.duckG.qna.service;
 
+import java.util.List;
+
+import com.duckG.vo.ProductVO;
+import com.duckG.vo.QnAVO;
+
 public interface QnAService {
+	List<QnAVO> selectQnA(int pno);
+	
+	boolean addQnA(QnAVO vo);
+
+	boolean delQnA(int pno);
+
+	boolean updateQnA(QnAVO vo);
 
 }
