f7ad6e38162a485105d1fc61365aac33130b2b22
BMJ <BMJ@203.228.77.59>
2024-07-01 23:49:25 +0900

    7/1 cart 수정

--- duckGduckG/src/main/java/com/duckG/cart/controller/SelectCart.java
+++ duckGduckG/src/main/java/com/duckG/cart/controller/SelectCart.java
@@ -22,38 +22,38 @@ public class SelectCart implements Control {
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		resp.setContentType("text/json;charset=utf-8");
-		
-		String id = req.getParameter("memberId");
-		
-		if(id != null){
-			CartService cso = new CartServiceImpl();
-			
-			List<CartVO> list = cso.cartList(id);
-			
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			String json = gson.toJson(list);
-			resp.getWriter().print(json);
-		} else {
-			try {
-				resp.setContentType("text/html; charset=utf-8");
-		        PrintWriter w = resp.getWriter();
-		        w.write("<script>alert('로그인좀');location.href='loginForm.do';</script>");
-		        w.flush();
-		        w.close();
-			} catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
-			}
-		}
-		
-		
-		HttpSession session = req.getSession();
-		
-		MemberVO mvo = (MemberVO)session.getAttribute("member");
-		
-		if(mvo == null) {
-			resp.sendRedirect("/loginForm.do");
-		}
+//		resp.setContentType("text/json;charset=utf-8");
+//		
+//		String id = req.getParameter("memberId");
+//		
+//		if(id != null){
+//			CartService cso = new CartServiceImpl();
+//			
+//			List<CartVO> list = cso.cartList(id);
+//			
+//			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//			String json = gson.toJson(list);
+//			resp.getWriter().print(json);
+//		} else {
+//			try {
+//				resp.setContentType("text/html; charset=utf-8");
+//		        PrintWriter w = resp.getWriter();
+//		        w.write("<script>alert('로그인좀');location.href='loginForm.do';</script>");
+//		        w.flush();
+//		        w.close();
+//			} catch (Exception e) {
+//				// TODO: handle exception
+//				e.printStackTrace();
+//			}
+//		}
+//		
+//		
+//		HttpSession session = req.getSession();
+//		
+//		MemberVO mvo = (MemberVO)session.getAttribute("member");
+//		
+//		if(mvo == null) {
+//			resp.sendRedirect("/loginForm.do");
+//		}
 	}
 }
