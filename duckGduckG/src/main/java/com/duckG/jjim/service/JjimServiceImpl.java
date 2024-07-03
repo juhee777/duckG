73d3491351b59b78dc06dc2e433681ba6843d954
admin <admin@YD01-11>
2024-07-03 17:37:34 +0900

    07:03 17:37 jjim

--- duckGduckG/src/main/java/com/duckG/jjim/service/JjimServiceImpl.java
+++ duckGduckG/src/main/java/com/duckG/jjim/service/JjimServiceImpl.java
@@ -1,13 +1,37 @@
 package com.duckG.jjim.service;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.duckG.product.mapper.ProductMapper;
-
+import com.duckG.vo.JjimVO;
 import com.duckG.DataSource;
+import com.duckG.jjim.mapper.JjimMapper;
 
 public class JjimServiceImpl implements JjimService{
 	SqlSession sqlSession = DataSource.getInstance().openSession(true);
-	ProductMapper mapper = sqlSession.getMapper(ProductMapper.class);
+	JjimMapper mapper = sqlSession.getMapper(JjimMapper.class);
+	
+	@Override
+	public List<JjimVO> selectJjim(int pno) {
+		// TODO Auto-generated method stub
+		return mapper.selectJjim(pno);
+	}
+	
+	@Override
+	public boolean addJjim(JjimVO jvo) {
+		// TODO Auto-generated method stub
+		return mapper.addJjim(jvo) == 1;
+	}
+	
+	@Override
+	public boolean delJjim(JjimVO jvo) {
+		// TODO Auto-generated method stub
+		return mapper.delJjim(jvo) == 1;
+	}
+	
+	
+	
 
 }
