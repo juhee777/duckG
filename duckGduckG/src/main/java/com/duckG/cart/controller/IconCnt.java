f92f8f82af00b893f33eee1421e7b15b15ed2d49
admin <admin@YD01-01>
2024-07-05 12:05:57 +0900

    7/5 cart 1

--- duckGduckG/src/main/java/com/duckG/cart/controller/IconCnt.java
+++ duckGduckG/src/main/java/com/duckG/cart/controller/IconCnt.java
@@ -11,25 +11,28 @@ import com.duckG.Control;
 import com.duckG.cart.service.CartService;
 import com.duckG.cart.service.CartServiceImpl;
 import com.duckG.vo.CartVO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-public class CheckCart implements Control {
+public class IconCnt implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		req.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/json;charset=UTF-8");
+		
 		CartService svc = new CartServiceImpl();
 		
 		HttpSession session = req.getSession();
 		String memberId = (String) session.getAttribute("logId");
-		int productNo = Integer.parseInt(req.getParameter("productNo"));
 		
 		CartVO cvo = new CartVO();
 		
 		cvo.setMemberId(memberId);
-		cvo.setProductNo(productNo);
 		
-		svc.checkCart(cvo);
+		Gson gson = new GsonBuilder().create();
+		
+		svc.iconCnt(cvo);
 	}
 }
--- duckGduckG/src/main/java/com/duckG/cart/mapper/CartMapper.java
+++ duckGduckG/src/main/java/com/duckG/cart/mapper/CartMapper.java
@@ -16,6 +16,9 @@ public interface CartMapper {
 
 	int checkCart(CartVO vco);// 이미 장바구니에 담긴 상품인지 확인
 	
-	int checkUpdateCart(CartVO cvo); // 장바구니에 있으면 업테이트
+	int checkUpdateCart(CartVO cvo); // 장바구니에 있으면 업데이트
+	
+	String IconCntCheck(CartVO vco); // 아이콘 장바구니 수량
+	
 
 }
